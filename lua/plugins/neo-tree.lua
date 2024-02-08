877dfa41ae4db1cd7d5a1290a16cb217515ccd43
João Alonso <joaoaalonso@gmail.com>
2024-02-08 11:37:31 -0300

    replace 'reveal' to 'toggle' on neotree keymap

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,20 +1,25 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("neo-tree").setup({
-			close_if_last_window = true,
-			popup_border_style = "rounded",
-			window = {
-				width = 30,
-			},
-		})
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      popup_border_style = "rounded",
+      window = {
+        width = 30,
+      },
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    })
 
-		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
-	end,
+    vim.keymap.set("n", "<leader>n", ":Neotree filesystem toggle left<CR>", {})
+  end,
 }
