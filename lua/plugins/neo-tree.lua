43c8692208ee1dc6a2a15020e7fbe02806679e7f
João Alonso <joaoaalonso@gmail.com>
2024-02-08 08:56:00 -0300

    set neo-tree width

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,20 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
-  end
-}
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			popup_border_style = "rounded",
+			window = {
+				width = 30,
+			},
+		})
 
+		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
+	end,
+}
